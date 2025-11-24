85a47460caba20715afe64e64e4101407d601d17
Mauricio Cerón <ceron.maw@gmail.com>
2025-11-24 10:43:45 -0600

    Refactor AssignmentsPage build method to enhance UI layout and display header

--- internal/ui/assignments_pages.go
+++ internal/ui/assignments_pages.go
@@ -25,6 +25,24 @@ func (p *AssignmentsPage) View() tview.Primitive {
 }
 
 func (p *AssignmentsPage) build() {
+	header := tview.NewTextView().
+		SetText("[::b]Assignments[::-]\nAsset assignments to employees").
+		SetDynamicColors(true)
+
+	content := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(header, 2, 0, false).
+		AddItem(nil, 1, 0, false)
+
 	p.view = tview.NewFlex().
-		AddItem(tview.NewTextView().SetText("Assignments Under Construction"), 0, 0, false)
+		SetDirection(tview.FlexRow).
+		AddItem(nil, 1, 0, false).
+		AddItem(
+			tview.NewFlex().
+				SetDirection(tview.FlexColumn).
+				AddItem(nil, 2, 0, false).
+				AddItem(content, 0, 1, true).
+				AddItem(nil, 2, 0, false),
+			0, 1, true).
+		AddItem(nil, 1, 0, false)
 }
