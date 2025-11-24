90ea30360a519748969208e7759850acb294828e
Mauricio Cerón <ceron.maw@gmail.com>
2025-11-24 13:23:03 -0600

    Add Asset struct definition to models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"time"
+)
+
+// Asset represents an IT asset in the inventory
+type Asset struct {
+	AssetID         string     `db:"asset_id"`
+	AssetCode       string     `db:"asset_code"`
+	TypeID          int        `db:"type_id"`
+	StatusID        int        `db:"status_id"`
+	SerialNumber    string     `db:"serial_number"`
+	Make            string     `db:"make"`
+	Model           string     `db:"model"`
+	Processor       *string    `db:"processor"`        // Nullable
+	RamGB           *int       `db:"ram_gb"`           // Nullable
+	StorageTB       *float64   `db:"storage_tb"`       // Nullable
+	StorageType     *string    `db:"storage_type"`     // Nullable (HDD, SSD, Hybrid)
+	OperatingSystem *string    `db:"operating_system"` // Nullable
+	PurchaseDate    time.Time  `db:"purchase_date"`
+	WarrantyEndDate *time.Time `db:"warranty_end_date"` // Nullable
+	LocationID      int        `db:"location_id"`
+	Notes           *string    `db:"notes"` // Nullable
+}
